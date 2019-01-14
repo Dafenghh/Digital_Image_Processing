89148b814ea4130e64e7191184cf92e9632bc8bc
Xu Dafeng <xudafeng88@qq.com>
2019-01-14 21:23:54 +0800

    dilation erosion result

--- assignments/homework5/part_1/Erosion.m
+++ assignments/homework5/part_1/Erosion.m
@@ -6,7 +6,7 @@ function [ res ] = Erosion( A, B, origin_x, origin_y )
             ok = 1;
             for i=1:size(B,1)
                 for j=1:size(B,2)
-                    if B(i,j) ~= GetVal(A, x + i - origin_x, y + j - origin_y)
+                    if B(i,j) == 1 &&  GetVal(A, x + i - origin_x, y + j - origin_y) == 0
                         ok = 0;
                         break;
                     end
